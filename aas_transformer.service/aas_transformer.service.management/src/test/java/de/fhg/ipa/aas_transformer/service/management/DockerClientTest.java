7bec0af1f6c60fae3c2c1d29f71b559c777ee2be
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-02-19 15:52:02 +0100

    add tests for wait for scale

--- aas_transformer.service/aas_transformer.service.management/src/test/java/de/fhg/ipa/aas_transformer/service/management/DockerClientTest.java
+++ aas_transformer.service/aas_transformer.service.management/src/test/java/de/fhg/ipa/aas_transformer/service/management/DockerClientTest.java
@@ -1,42 +1,62 @@
 package de.fhg.ipa.aas_transformer.service.management;
 
+import com.github.dockerjava.api.model.Service;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@SpringBootTest(classes = DockerHandler.class)
+
+@SpringBootTest(classes = TransformerServiceHandler.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Disabled
 public class DockerClientTest {
     @Autowired
-    DockerHandler dockerHandler;
+    TransformerServiceHandler transformerServiceHandler;
 
     @Test
     @Order(10)
     public void testGetExecutorCountExpectOne() {
-        long executorCount = dockerHandler.getReplicaCountOfExecutorService();
+        long executorCount = transformerServiceHandler.getReplicaCountOfExecutorService();
         assert(executorCount == 1);
     }
 
     @Test
     @Order(20)
-    public void testScaleUpExecutorExpectTwo() {
-        dockerHandler.scaleExecutorService(2);
-        assert(dockerHandler.getReplicaCountOfExecutorService() == 2);
+    public void testScaleUpExecutorExpectTwo() throws DockerHandler.WaitForScaleTimeoutException {
+        transformerServiceHandler.scaleExecutorService(2, false);
+        assert(transformerServiceHandler.getReplicaCountOfExecutorService() == 2);
     }
-
+    
     @Test
     @Order(30)
-    public void testScaleDownExecutorExpectOne() {
-        dockerHandler.scaleExecutorService(1);
-        assert(dockerHandler.getReplicaCountOfExecutorService() == 1);
+    public void testWaitForExecutorScaleUpFinished() {
+        assertDoesNotThrow(() -> {
+            transformerServiceHandler.waitExecutorScaleToFinish();
+        });
     }
 
     @Test
     @Order(40)
-    public void testScaleDownToZeroExpectOne() {
-        dockerHandler.scaleExecutorService(0);
-        assert(dockerHandler.getReplicaCountOfExecutorService() == 1);
+    public void testScaleUpExecutorAndWaitExpectNoException() throws DockerHandler.WaitForScaleTimeoutException {
+        assertDoesNotThrow(() -> {
+            transformerServiceHandler.scaleExecutorService(3, true);
+        });
+        assert(transformerServiceHandler.getReplicaCountOfExecutorService() == 3);
+    }
+
+    @Test
+    @Order(50)
+    public void testScaleDownExecutorExpectOne() throws DockerHandler.WaitForScaleTimeoutException {
+        transformerServiceHandler.scaleExecutorService(1, false);
+        assert(transformerServiceHandler.getReplicaCountOfExecutorService() == 1);
+    }
+
+    @Test
+    @Order(60)
+    public void testScaleDownToZeroExpectOne() throws DockerHandler.WaitForScaleTimeoutException {
+        transformerServiceHandler.scaleExecutorService(0, false);
+        assert(transformerServiceHandler.getReplicaCountOfExecutorService() == 1);
     }
 }
