a348d834812d45dd59d494d566444b1fe48d3574
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-02-21 20:16:18 +0100

    init

---
+++ aas_transformer.service/aas_transformer.service.executor/src/main/java/de/fhg/ipa/aas_transformer/service/executor/ExecutorRestController.java
@@ -0,0 +1,23 @@
+package de.fhg.ipa.aas_transformer.service.executor;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("")
+public class ExecutorRestController {
+    @Autowired
+    TransformationExecutionServiceCache transformationExecutionServiceCache;
+
+    @RequestMapping(path = "/transformer-id-list", method = RequestMethod.GET)
+    @Operation(summary = "Get cached transformers as list of ids")
+    public List<UUID> getTransformerIds() {
+        return transformationExecutionServiceCache.getTransformerIds();
+    }
+}
---
+++ aas_transformer.test/aas_transformer.test.system/src/test/java/de/fhg/ipa/aas_transformer/test/system/RestartSystemTest.java
@@ -0,0 +1,97 @@
+package de.fhg.ipa.aas_transformer.test.system;
+
+import de.fhg.ipa.aas_transformer.clients.management.ManagementClient;
+import de.fhg.ipa.aas_transformer.model.Transformer;
+import de.fhg.ipa.aas_transformer.model.TransformerDTOListener;
+import de.fhg.ipa.aas_transformer.test.utils.extentions.AasITExtension;
+import de.fhg.ipa.aas_transformer.test.utils.extentions.AasTransformerExtension;
+import de.fhg.ipa.aas_transformer.test.utils.extentions.MariaDbExtension;
+import de.fhg.ipa.aas_transformer.test.utils.extentions.RedisExtension;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.UUID;
+
+import static de.fhg.ipa.aas_transformer.test.utils.TransformerTestObjects.getTimeseriesTransformer;
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MariaDbExtension.class)
+@ExtendWith(AasITExtension.class)
+@ExtendWith(RedisExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Disabled
+public class RestartSystemTest {
+    @RegisterExtension
+    static AasTransformerExtension aasTransformerExtension = new AasTransformerExtension(
+            true, true, true
+    );
+
+    // Service Addresses:
+    String transformerManagementPort = System.getProperty("aas_transformer.services.management.port");
+    String transformerListenerPort = System.getProperty("aas_transformer.services.listener.port");
+    String transformerExecutorPort = System.getProperty("aas_transformer.services.executor.port");
+
+    // Clients:
+    ManagementClient managementClient;
+    WebClient listenerClient;
+    WebClient executorClient;
+
+    // Test objects:
+    Transformer timeseriesTransformer = getTimeseriesTransformer("test_timeseries");
+
+    @BeforeEach
+    public  void beforeEach() {
+        managementClient = new ManagementClient("http://localhost:" + transformerManagementPort);
+        listenerClient = WebClient.create("http://localhost:" + transformerListenerPort);
+        executorClient = WebClient.create("http://localhost:" + transformerExecutorPort);
+        // create Transformer
+        managementClient.createTransformer(timeseriesTransformer, false).block();
+    }
+
+    @Test
+    @Order(10)
+    public void testRestartManagementExpectExecutorListenerReconnect() throws InterruptedException {
+        List<Transformer> transformer = managementClient.getAllTransformer().collectList().block();
+        assertTrue( transformer.size() == 1 );
+
+        // check transformer available @ Listener/Executor
+
+        aasTransformerExtension.restartManagementContainer();
+
+        // delete transformer
+        transformer.forEach(
+                t -> managementClient.deleteTransformer(t.getId(), false).block()
+        );
+
+        sleep(500);
+
+        transformer = managementClient.getAllTransformer().collectList().block();
+        assertTrue( transformer.size() == 0 );
+
+        // check transformer not available @ Listener anymore
+        // => reconnect after restart successful
+        List<TransformerDTOListener> transformerDtoListener = listenerClient.get()
+                .uri("/transformer-cache")
+                .retrieve()
+                .bodyToFlux(TransformerDTOListener.class)
+                .collectList()
+                .block();
+
+        assertTrue(transformerDtoListener.size() == 0);
+
+        // check transformer not available @ Executor anymore
+        // => reconnect after restart successful
+        List<UUID> transformerIds = executorClient.get()
+                .uri("/transformer-id-list")
+                .retrieve()
+                .bodyToFlux(UUID.class)
+                .collectList()
+                .block();
+
+        assertTrue(transformerIds.size() == 0);
+    }
+}
