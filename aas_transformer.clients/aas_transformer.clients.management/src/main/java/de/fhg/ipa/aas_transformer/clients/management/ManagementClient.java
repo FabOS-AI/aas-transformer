013d919f47c2cb9cf8954a62b0f53a1e17e8f7c2
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-02 03:37:20 +0100

    fix init of apiclient

--- aas_transformer.clients/aas_transformer.clients.management/src/main/java/de/fhg/ipa/aas_transformer/clients/management/ManagementClient.java
+++ aas_transformer.clients/aas_transformer.clients.management/src/main/java/de/fhg/ipa/aas_transformer/clients/management/ManagementClient.java
@@ -5,8 +5,12 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import de.fhg.ipa.aas_transformer.clients.ApiClient;
 import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
@@ -17,6 +21,7 @@ import java.time.Duration;
 public class ManagementClient extends TransformerRestControllerApi {
     private static ObjectMapper objectMapper;
     private static WebClient webClient;
+    private static ApiClient apiClient;
     static {
         objectMapper = new ObjectMapper();
         SimpleModule simpleModule = new SimpleModule();
@@ -35,17 +40,23 @@ public class ManagementClient extends TransformerRestControllerApi {
         httpClient.warmup().block();
 
         var reactorClientHttpConnector = new ReactorClientHttpConnector(httpClient);
+
+        ExchangeStrategies strategies = ExchangeStrategies
+                .builder()
+                .codecs(clientDefaultCodecsConfigurer -> {
+                    clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON));
+                    clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON));
+                }).build();
+
         webClient = WebClient.builder()
                 .clientConnector(reactorClientHttpConnector)
+                .exchangeStrategies(strategies)
                 .build();
+
+        apiClient = new ApiClient(webClient);
     }
 
     public ManagementClient(@Value("${aas_transformer.services.management.base-url}") String baseUrl) {
-        super(new ApiClient(
-                webClient,
-                objectMapper,
-                ApiClient.createDefaultDateFormat()
-            ).setBasePath(baseUrl)
-        );
+        super(apiClient.setBasePath(baseUrl));
     }
 }
