a3dcca10971c9d2cdc61d58a0c586d03d16593cd
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-05 22:00:49 +0100

    add alertstatus class

--- aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/alertmanager/Alert.kt
+++ aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/alertmanager/Alert.kt
@@ -3,11 +3,17 @@ package de.fhg.ipa.aas_transformer.model.alertmanager
 import java.time.OffsetDateTime
 
 data class Alert(
-    val status: String,
-    val labels: Map<String, String>,
-    val annotations: Map<String, String>,
-    val startsAt: OffsetDateTime,
-    val endsAt: OffsetDateTime,
-    val generatorURL: String,
-    val fingerprint: String
-)
+    val status: AlertStatus = AlertStatus(),
+    val labels: Map<String, String> = emptyMap(),
+    val annotations: Map<String, String> = emptyMap(),
+    val startsAt: OffsetDateTime? = null,
+    val updatedAt: OffsetDateTime? = null,
+    val endsAt: OffsetDateTime? = null,
+    val receivers: List<Map<String,String>> = emptyList(),
+    val generatorURL: String = "",
+    val fingerprint: String = ""
+) {
+    fun getAlertname(): String {
+        return labels["alertname"] ?: ""
+    }
+}
