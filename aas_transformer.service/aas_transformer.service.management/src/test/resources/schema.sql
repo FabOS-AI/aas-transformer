9daca5d0e8ff4b8d01f5e45cbc0e389c3e7917d2
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-09 19:46:57 +0100

    add transform on request

--- aas_transformer.service/aas_transformer.service.management/src/test/resources/schema.sql
+++ aas_transformer.service/aas_transformer.service.management/src/test/resources/schema.sql
@@ -4,6 +4,7 @@ create table if not exists transformer
     destination                 longtext   null,
     transformer_actions         longtext   null,
     source_submodel_id_rules    longtext   null,
+    transform_on_request        bool       default false,
     optlock                     int        null,
     version                     int        null
 );
