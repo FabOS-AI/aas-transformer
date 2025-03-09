e0ca873542c4208c8478874ce4b054e344c74993
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-09 20:18:41 +0100

    add defaults for properties

--- aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/alertmanager/AlertMessage.kt
+++ aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/alertmanager/AlertMessage.kt
@@ -1,14 +1,14 @@
 package de.fhg.ipa.aas_transformer.model.alertmanager
 
 data class AlertMessage(
-    val receiver: String,
-    val status: String,
-    val alerts: List<Alert>,
-    val groupLabels: Map<String, String>,
-    val commonLabels: Map<String, String>,
-    val commonAnnotations: Map<String, String>,
-    val externalURL: String,
-    val version: String,
-    val groupKey: String,
-    val truncatedAlerts: Int
+    val receiver: String = "",
+    val status: String = "",
+    val alerts: List<Alert> = emptyList(),
+    val groupLabels: Map<String, String> = emptyMap(),
+    val commonLabels: Map<String, String> = emptyMap(),
+    val commonAnnotations: Map<String, String> = emptyMap(),
+    val externalURL: String = "",
+    val version: String = "",
+    val groupKey: String = "",
+    val truncatedAlerts: Int = 0
 )
