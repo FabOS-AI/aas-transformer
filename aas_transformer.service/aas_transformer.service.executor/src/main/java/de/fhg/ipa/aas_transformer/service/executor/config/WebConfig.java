c9108c27fbacbd06847ed3c158766644e70369fa
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-11 16:58:20 +0100

    use serialization extension

--- aas_transformer.service/aas_transformer.service.executor/src/main/java/de/fhg/ipa/aas_transformer/service/executor/config/WebConfig.java
+++ aas_transformer.service/aas_transformer.service.executor/src/main/java/de/fhg/ipa/aas_transformer/service/executor/config/WebConfig.java
@@ -1,10 +1,12 @@
 package de.fhg.ipa.aas_transformer.service.executor.config;
 
-import de.fhg.ipa.aas_transformer.service.executor.converter.DefaultSubmodelConverter;
-import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.eclipse.digitaltwin.basyx.http.Aas4JHTTPSerializationExtension;
+import org.eclipse.digitaltwin.basyx.http.SerializationExtension;
+import org.eclipse.digitaltwin.basyx.submodelrepository.http.SubmodelRepositoryHTTPSerializationExtension;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,12 +14,22 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Bean
-    public HttpMessageConverter<Submodel> defaultSubmodelHttpMessageConverter() {
-        return new DefaultSubmodelConverter();
+    public SubmodelRepositoryHTTPSerializationExtension submodelRepositoryHTTPSerializationExtension() {
+        return new SubmodelRepositoryHTTPSerializationExtension();
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(defaultSubmodelHttpMessageConverter());
+    @Bean
+    public Aas4JHTTPSerializationExtension aas4JHTTPSerializationExtension() {
+        return new Aas4JHTTPSerializationExtension();
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder(List<SerializationExtension> serializationExtensions) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().serializationInclusion(JsonInclude.Include.NON_NULL);
+
+        for (SerializationExtension serializationExtension : serializationExtensions) {
+            serializationExtension.extend(builder);
+        }
+        return builder;
     }
 }
