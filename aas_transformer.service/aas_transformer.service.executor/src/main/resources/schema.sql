69b3ca0a7f8eb35203faed7741d9a75228fa196c
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-12 17:29:10 +0100

    add unique on columns

--- aas_transformer.service/aas_transformer.service.executor/src/main/resources/schema.sql
+++ aas_transformer.service/aas_transformer.service.executor/src/main/resources/schema.sql
@@ -3,5 +3,6 @@ create table if not exists transformation_description
     id                          int auto_increment primary key,
     transformer_id              varchar(37) not null,
     source_submodel_id          text not null,
-    target_submodel_id          text not null
+    target_submodel_id          text not null,
+    UNIQUE (transformer_id, source_submodel_id, target_submodel_id)
 );
