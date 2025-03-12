a706d9a0dd506a0d0d01b57bbeda5c7c8b487614
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-12 20:13:57 +0100

    cleanup

--- aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/TransformationDescription.kt
+++ aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/TransformationDescription.kt
@@ -1,9 +1,6 @@
 package de.fhg.ipa.aas_transformer.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.util.*
 
 @Entity
