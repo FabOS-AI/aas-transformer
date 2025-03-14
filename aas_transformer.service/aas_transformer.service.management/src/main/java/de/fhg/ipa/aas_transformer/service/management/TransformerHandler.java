cbf0c3e55d6c319b57ef1cfc9839eeedac1d5fe9
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-14 17:16:25 +0100

    add missing subscribe

--- aas_transformer.service/aas_transformer.service.management/src/main/java/de/fhg/ipa/aas_transformer/service/management/TransformerHandler.java
+++ aas_transformer.service/aas_transformer.service.management/src/main/java/de/fhg/ipa/aas_transformer/service/management/TransformerHandler.java
@@ -151,17 +151,20 @@ public class TransformerHandler {
                     .publishOn(Schedulers.boundedElastic())
                     .doOnSuccess(v -> {
                         emitToSink(new TransformerChangeEvent(TransformerChangeEventType.DELETE, t));
+                        LOG.info("Deleted transformer with ID: {}", transformerId);
                     })
                     .subscribe();
-                LOG.info("Deleted transformer with ID: {}", transformerId);
                 this.transformationDescriptionJpaRepository
                     .findByTransformerId(transformerId)
                     .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(1)))
                     .subscribe(td -> {
                         this.submodelRegistry.deleteSubmodelDescriptor(td.getTargetSubmodelId());
-                        this.transformationDescriptionJpaRepository.delete(td).doOnSuccess(v ->
-                            LOG.info("Deleted transformation description with ID: {}", td.getId())
-                        );
+                        this.transformationDescriptionJpaRepository
+                                .delete(td)
+                                .doOnSuccess(v ->
+                                        LOG.info("Deleted transformation description with ID: {}", td.getId())
+                                )
+                                .subscribe();
                     });
             });
     }
