6b07ff09159a35948b177d1c03192da77559e129
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-14 20:30:13 +0100

    init

---
+++ aas_transformer.clients/aas_transformer.clients.management/src/main/java/de/fhg/ipa/aas_transformer/clients/management/SubmodelElementSerializer.java
@@ -0,0 +1,28 @@
+package de.fhg.ipa.aas_transformer.clients.management;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.SerializationException;
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer;
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
+
+import java.io.IOException;
+
+public class SubmodelElementSerializer extends StdSerializer<SubmodelElement> {
+    JsonSerializer jsonSerializer = new JsonSerializer();
+
+    public SubmodelElementSerializer(Class<SubmodelElement> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(SubmodelElement submodel, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        try {
+            jsonGenerator.writeRaw(jsonSerializer.write(submodel));
+        } catch (SerializationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ aas_transformer.clients/aas_transformer.clients.redis/src/main/java/de/fhg/ipa/aas_transformer/clients/redis/SubmodelElementDeserializer.java
@@ -0,0 +1,39 @@
+package de.fhg.ipa.aas_transformer.clients.redis;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.DeserializationException;
+import org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonDeserializer;
+import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
+
+import java.io.IOException;
+
+public class SubmodelElementDeserializer extends StdDeserializer<SubmodelElement> {
+    static ObjectMapper mapper = new ObjectMapper();
+    JsonDeserializer jsonDeserializer = new JsonDeserializer();
+
+    public SubmodelElementDeserializer() {
+        this(null);
+    }
+
+    public SubmodelElementDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public SubmodelElement deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        try {
+            JsonNode node = mapper.readTree(p);
+            String nodeText = mapper.writeValueAsString(node);
+            return jsonDeserializer.read(nodeText, SubmodelElement.class);
+        } catch (DeserializationException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
