411e672b3f5490464beb5a32455db80b656b3614
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-16 22:48:29 +0100

    add crossorigin

--- aas_transformer.service/aas_transformer.service.executor/src/main/java/de/fhg/ipa/aas_transformer/service/executor/controller/SubmodelRepositoryRestController.java
+++ aas_transformer.service/aas_transformer.service.executor/src/main/java/de/fhg/ipa/aas_transformer/service/executor/controller/SubmodelRepositoryRestController.java
@@ -7,10 +7,7 @@ import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +15,7 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @RestController
 @RequestMapping("/submodels")
+@CrossOrigin(origins = "*")
 @Produces(APPLICATION_JSON)
 public class SubmodelRepositoryRestController {
     private static final Logger LOG = LoggerFactory.getLogger(SubmodelRepositoryRestController.class);
