30a0d2398a14b6a983fb067ea8b34838e981e71b
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-03-27 18:25:27 +0100

    add loop for getting potential sourceSubmodel if sourceSubmodel has template in id

--- aas_transformer.transformation/src/main/java/de/fhg/ipa/aas_transformer/transformation/TransformationDetectionService.java
+++ aas_transformer.transformation/src/main/java/de/fhg/ipa/aas_transformer/transformation/TransformationDetectionService.java
@@ -2,10 +2,13 @@ package de.fhg.ipa.aas_transformer.transformation;
 
 import de.fhg.ipa.aas_transformer.aas.AasRegistry;
 import de.fhg.ipa.aas_transformer.aas.AasRepository;
+import de.fhg.ipa.aas_transformer.aas.SubmodelRegistry;
+import de.fhg.ipa.aas_transformer.aas.SubmodelRepository;
 import de.fhg.ipa.aas_transformer.model.TransformerDTOListener;
 import de.fhg.ipa.aas_transformer.transformation.templating.TemplateRenderer;
 import org.eclipse.digitaltwin.aas4j.v3.model.AssetAdministrationShell;
 import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
+import org.eclipse.digitaltwin.basyx.submodelregistry.client.model.SubmodelDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +24,21 @@ public class TransformationDetectionService {
     private final TransformerDTOListener transformerDTOListener;
     private final TemplateRenderer templateRenderer;
     private final TransformationUtils transformationUtils;
+    private final AasRegistry aasRegistry;
+    private final SubmodelRegistry submodelRegistry;
 
     public TransformationDetectionService(
             TransformerDTOListener transformerDTOListener,
             TemplateRenderer templateRenderer,
-            TransformationUtils transformationUtils
+            TransformationUtils transformationUtils,
+            AasRegistry aasRegistry,
+            SubmodelRegistry submodelRegistry
     ) {
         this.transformerDTOListener = transformerDTOListener;
         this.templateRenderer = templateRenderer;
         this.transformationUtils = transformationUtils;
+        this.aasRegistry = aasRegistry;
+        this.submodelRegistry = submodelRegistry;
     }
     public boolean isSubmodelSourceOfTransformerActions(Submodel submodel) {
         return isSubmodelSourceOfTransformer(
@@ -38,23 +47,39 @@ public class TransformationDetectionService {
         );
     }
 
-    public boolean isSubmodelDestinationOfTransformerAction(Submodel submodel) {
+    public boolean isSubmodelDestinationOfTransformerAction(Submodel sourceSubmodel) {
+        String destinationId = transformerDTOListener.getDestination().getSubmodelDestination().getId();
+
+        if(!TemplateRenderer.hasTemplate(destinationId))
+            return (destinationId != null && destinationId == sourceSubmodel.getId());
+
+        for(SubmodelDescriptor smd : submodelRegistry.getSubmodelDescriptors()) {
+            Submodel potentialSourceSubmodel = SubmodelRepository.getExtSubmodel(submodelRegistry, smd.getId());
+            if(isSubmodelTransformationSourceOfSubmodel(potentialSourceSubmodel, sourceSubmodel)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSubmodelTransformationSourceOfSubmodel(Submodel sourceSubmodel, Submodel destinationSubmodel) {
         List<AssetAdministrationShell> destinationShells = transformationUtils.lookupDestinationShells(
-                submodel.getId(),
+                sourceSubmodel.getId(),
                 transformerDTOListener.getDestination().getAasDestination(),
-                templateRenderer.getTemplateContext(transformerDTOListener.getId(),List.of(), submodel)
+                templateRenderer.getTemplateContext(transformerDTOListener.getId(),List.of(), sourceSubmodel)
         );
         Map<String, Object> context = templateRenderer.getTemplateContext(
                 transformerDTOListener.getId(),
                 destinationShells,
-                submodel
+                sourceSubmodel
         );
-        String destinationId = templateRenderer.render(
+        String potentialDestinationId = templateRenderer.render(
                 transformerDTOListener.getDestination().getSubmodelDestination().getId(),
                 context
         );
 
-        return (destinationId != null && destinationId == submodel.getId());
+        return potentialDestinationId.equals(destinationSubmodel.getId());
     }
 
     public TransformerDTOListener getTransformerDTOListener() {
