6998db6240205140a32d13333aebdf679282afd8
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-07-18 20:29:52 +0200

    use jsonSerializer and writeTree()

--- aas_transformer.aas/src/main/java/de/fhg/ipa/aas_transformer/aas/serializer/SubmodelSerializer.java
+++ aas_transformer.aas/src/main/java/de/fhg/ipa/aas_transformer/aas/serializer/SubmodelSerializer.java
@@ -1,27 +1,21 @@
 package de.fhg.ipa.aas_transformer.aas.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.SerializationException;
 import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
-import org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer;
+//import org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer;
 
 import java.io.IOException;
 
-public class SubmodelSerializer extends StdSerializer<Submodel> {
-    JsonSerializer jsonSerializer = new JsonSerializer();
-
-    public SubmodelSerializer(Class<Submodel> t) {
-        super(t);
-    }
+public class SubmodelSerializer extends JsonSerializer<Submodel> {
+    org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer jsonSerializer = new org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer();
 
     @Override
     public void serialize(Submodel submodel, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        try {
-            jsonGenerator.writeRaw(jsonSerializer.write(submodel));
-        } catch (SerializationException e) {
-            throw new RuntimeException(e);
-        }
+            jsonGenerator.writeTree(jsonSerializer.toNode(submodel));
     }
 }
