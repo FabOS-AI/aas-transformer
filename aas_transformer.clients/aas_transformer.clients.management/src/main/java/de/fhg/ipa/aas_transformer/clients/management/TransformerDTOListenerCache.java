8690056129f6af08660dad7e3699e068d3c27132
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-07-22 15:12:42 +0200

    cleanup

--- aas_transformer.clients/aas_transformer.clients.management/src/main/java/de/fhg/ipa/aas_transformer/clients/management/TransformerDTOListenerCache.java
+++ aas_transformer.clients/aas_transformer.clients.management/src/main/java/de/fhg/ipa/aas_transformer/clients/management/TransformerDTOListenerCache.java
@@ -2,13 +2,9 @@ package de.fhg.ipa.aas_transformer.clients.management;
 
 import de.fhg.ipa.aas_transformer.model.TransformerChangeEventDTOListener;
 import de.fhg.ipa.aas_transformer.model.TransformerDTOListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
 public abstract class TransformerDTOListenerCache {
-    private static final Logger LOG = LoggerFactory.getLogger(TransformerDTOListenerCache.class);
-
     protected final int connectionRetryTimeoutInMs = 1000;
     protected final ManagementClient managementClient;
     protected Flux<TransformerDTOListener> transformerFlux;
