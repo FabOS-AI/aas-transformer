38a7d5dc9a6a0a8a2ee0a2d144ffe9f8b3976773
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-07-29 19:58:09 +0200

    add default constructor

--- aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/TransformationJob.kt
+++ aas_transformer.model/src/main/java/de/fhg/ipa/aas_transformer/model/TransformationJob.kt
@@ -4,12 +4,14 @@ import org.eclipse.digitaltwin.aas4j.v3.model.Submodel
 import java.util.UUID
 
 data class TransformationJob(
-    var transformationJobAction: TransformationJobAction,
+    var transformationJobAction: TransformationJobAction?,
     var transformerId: UUID?,
     var submodelId: String?,
     var submodel: Submodel?,
     var targetSubmodelId: String?
 ) {
+    constructor() : this(null, null, null, null, null)
+
     override fun toString(): String {
         return "TransformationJob(transformationJobAction=$transformationJobAction, transformerId=$transformerId, submodelId=$submodelId, targetSubmodelId=$targetSubmodelId)"
     }
