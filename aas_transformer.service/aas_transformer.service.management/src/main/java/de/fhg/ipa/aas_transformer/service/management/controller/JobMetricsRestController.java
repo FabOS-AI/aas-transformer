bda5645eea6ac9f0a200f8553f1c1eb446fe87e4
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-07-29 20:03:37 +0200

    rename

--- aas_transformer.service/aas_transformer.service.management/src/main/java/de/fhg/ipa/aas_transformer/service/management/controller/JobMetricsRestController.java
+++ aas_transformer.service/aas_transformer.service.management/src/main/java/de/fhg/ipa/aas_transformer/service/management/controller/JobMetricsRestController.java
@@ -1,6 +1,6 @@
 package de.fhg.ipa.aas_transformer.service.management.controller;
 
-import de.fhg.ipa.aas_transformer.clients.redis.RedisJobReader;
+import de.fhg.ipa.aas_transformer.clients.redis.RedisJobConsumer;
 import de.fhg.ipa.aas_transformer.model.TransformationJob;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,37 +12,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/transformer-jobs")
-public class JobRestController {
+public class JobMetricsRestController {
     @Autowired
-    RedisJobReader redisJobReader;
+    RedisJobConsumer redisJobConsumer;
 
     @RequestMapping(path = "/waiting", method = RequestMethod.GET)
     @Operation(summary = "Get all waiting jobs")
     public List<TransformationJob> getWaitingJobs() {
-        return redisJobReader.getWaitingJobs();
+        return redisJobConsumer.getWaitingJobs();
     }
 
     @RequestMapping(path = "/in-progress", method = RequestMethod.GET)
     @Operation(summary = "Get all in-progress jobs")
     public List<TransformationJob> getInProgressJobs() {
-        return redisJobReader.getInProgressJobs();
+        return redisJobConsumer.getInProgressJobs();
     }
 
     @RequestMapping(path = "/count/total", method = RequestMethod.GET)
     @Operation(summary = "Get count of all jobs")
     public int getTotalJobCount() {
-        return redisJobReader.getTotalJobCount();
+        return redisJobConsumer.getTotalJobCount();
     }
 
     @RequestMapping(path = "/count/waiting", method = RequestMethod.GET)
     @Operation(summary = "Get count of waiting jobs")
     public long getWaitingJobCount() {
-        return redisJobReader.getWaitingJobCount();
+        return redisJobConsumer.getWaitingJobCount();
     }
 
     @RequestMapping(path = "/count/in-progress", method = RequestMethod.GET)
     @Operation(summary = "Get count of in-progress jobs")
     public long getInProgressJobCount() {
-        return redisJobReader.getInProgressJobCount();
+        return redisJobConsumer.getInProgressJobCount();
     }
 }
