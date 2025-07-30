50583dd53d21fee43fdedc8a56ce03ffeb9f4ee0
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-07-30 17:34:18 +0200

    cleanup

--- aas_transformer.aas/src/main/java/de/fhg/ipa/aas_transformer/aas/serializer/SubmodelSerializer.java
+++ aas_transformer.aas/src/main/java/de/fhg/ipa/aas_transformer/aas/serializer/SubmodelSerializer.java
@@ -1,13 +1,9 @@
 package de.fhg.ipa.aas_transformer.aas.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.SerializationException;
 import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
-//import org.eclipse.digitaltwin.aas4j.v3.dataformat.json.JsonSerializer;
 
 import java.io.IOException;
 
