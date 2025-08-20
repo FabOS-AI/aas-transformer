4ad0a47bc8db318bc7095db101267a3f9d538e1f
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-08-20 21:18:47 +0200

    refactor test: run test with variable listener count, analyze created jobs over time

--- aas_transformer.test/aas_transformer.test.system/src/test/java/de/fhg/ipa/aas_transformer/test/system/performance/listener/HistoricDataListenerTest.java
+++ aas_transformer.test/aas_transformer.test.system/src/test/java/de/fhg/ipa/aas_transformer/test/system/performance/listener/HistoricDataListenerTest.java
@@ -1,13 +1,18 @@
 package de.fhg.ipa.aas_transformer.test.system.performance.listener;
 
+import de.fhg.ipa.aas_transformer.clients.management.RedisControllerClient;
 import de.fhg.ipa.aas_transformer.model.ServiceType;
 import de.fhg.ipa.aas_transformer.model.Transformer;
 import de.fhg.ipa.aas_transformer.test.utils.creator.HistoricDataCreator;
+import de.fhg.ipa.aas_transformer.test.utils.creator.TimeSeriesSubmodelCreator;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -22,28 +27,47 @@ class HistoricDataListenerTest extends AbstractListenerTest {
 
 
     @Order(10)
-    @Test
-    public void testScaleUpOfListener() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, })
+    public void testScaleUpOfListener(int listenerCount) throws InterruptedException {
         int submodelCreatorCount = 4;
+        int testDurationInMs = 30 * 1000;
 
         // Scale Listener:
-        System.out.println("Scale Listener to 1 replica");
-        scalingClient.scaleServiceType(ServiceType.LISTENER, 1L, false).block();
-        waitForListenerCount(count -> count != 1);
+        System.out.println("Scale Listener to "+listenerCount+" replica");
+        scalingClient.enableServiceType(ServiceType.LISTENER, true).block();
+        scalingClient.scaleServiceType(ServiceType.LISTENER, (long) listenerCount, false).block();
+        scalingClient.enableServiceType(ServiceType.LISTENER, false).block();
+        waitForListenerCount(count -> count != listenerCount);
 
         // Create and Start HistoricDataCreator Threads:
         List<HistoricDataCreator> smCreatorThreads = new ArrayList<>();
         for(int i = 0; i < submodelCreatorCount; i++)
             smCreatorThreads.add(createHistoricDataCreator(0, 1));
 
-        smCreatorThreads.forEach(t -> t.start());
+        smCreatorThreads.forEach(TimeSeriesSubmodelCreator::start);
 
-        // Wait Listener to scale up:
-        waitForListenerCount(count -> count == 1);
-
-        // TODO: Measure/Compare performance of 1 vs. 2 Listener instances
+        sleep(testDurationInMs);
 
         // Stop Submodel Creators:
-        smCreatorThreads.forEach(t -> t.stop());
+        smCreatorThreads.forEach(TimeSeriesSubmodelCreator::stop);
+        redisControllerClient.deleteMessageEvents().block();
+
+        scalingClient.enableServiceType(ServiceType.LISTENER, true).block();
+        scalingClient.scaleServiceType(ServiceType.LISTENER, 0L, false).block();
+        scalingClient.enableServiceType(ServiceType.LISTENER, false).block();
+        Long createdJobCount = redisControllerClient.getWaitingJobCount().block();
+
+        System.out.println("Listener Count: " + listenerCount);
+        System.out.println("Submodel Creator Count: " + submodelCreatorCount);
+        System.out.println("Created Jobs: " + createdJobCount);
+        System.out.println("Test duration: " + testDurationInMs / 1000 + "s");
+        clearRedis();
+    }
+
+    private void clearRedis() {
+        redisControllerClient.deleteInProgressJobs().block();
+        redisControllerClient.deleteWaitingJobs().block();
+        redisControllerClient.deleteMessageEvents().block();
     }
 }
